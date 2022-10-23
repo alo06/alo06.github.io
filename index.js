fd2207c97928c335c80ed680aab1513b59053384
alo06 <96821629+alo06@users.noreply.github.com>
2022-10-23 12:11:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+let home= document.querySelector('#moon-btn');
+
+document.querySelector('#moon-btn').onclick = () =>{
+   document.documentElement.classList.toggle('dark-mode');
+   document.querySelectorAll('.inverted').forEach((result) => {
+    result.classList.toggle('invert');
+   })
+}
+
